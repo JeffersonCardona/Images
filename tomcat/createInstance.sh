8f869ab742b342106555991577882a21fd1ef952
jacardona <jacardona@localhost.localdomain>
2024-10-13 20:51:05 -0500

    Ajuste para incluir 8080

--- tomcat/createInstance.sh
+++ tomcat/createInstance.sh
@@ -7,4 +7,4 @@ SETTINGS=$4
 LOGS=$5
 WEBAPPS=$6
 
-docker run -d --privileged --name $NAME --memory 2g --net $NETWORK -e TZ=America/Bogota -v $SETTINGS:/home/settings -v $LOGS:/home/apache-tomcat/logs -v $WEBAPPS:/home/apache-tomcat/webapps localhost/tomcat:$VERSION
+docker run -d --privileged --name $NAME --memory 2g --net $NETWORK -p 8080:8080 -e TZ=America/Bogota -v $SETTINGS:/home/settings -v $LOGS:/home/apache-tomcat/logs -v $WEBAPPS:/home/apache-tomcat/webapps localhost/tomcat:$VERSION
